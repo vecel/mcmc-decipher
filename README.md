bb4a94dfdc17d26d3c4cae5d917429ee824b1716
vecel <mateusz.karandys@gmail.com>
2025-04-24 21:08:44 +0200

    Update README.md, add cipher utils

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # MCMC Decipher
-An exploration of using Monte Carlo Markov Chain (MCMC) methods to break substitution ciphers - a classic encryption technique where each letter is replaced with another. This project demonstrates how probabilistic modeling and MCMC sampling can be applied to decrypt encoded text by learning likely letter mappings based on language patterns.
+<div align="justify">
+An exploration of using Monte Carlo Markov Chain (MCMC) methods to break substitution ciphers - a classic encryption technique where each letter is replaced with another. This project demonstrates how probabilistic modeling and MCMC sampling can be applied to decrypt encoded text by learning likely letter mappings based on language patterns. For the introduction to the project we recommend to read this <a href="https://medium.com/data-science/breaking-the-enigma-code-in-python-with-mcmc-marvel-themed-9ceb358dd8ae">article</a> by Jack J on Medium.
+</div>
 
 ### Sources
-[Blog post by Jack J on Medium](https://medium.com/data-science/breaking-the-enigma-code-in-python-with-mcmc-marvel-themed-9ceb358dd8ae)
-[Author's Github repository](https://github.com/JackWillz/Projects/tree/master/MCMC%20-%20Enigma%20Thanos)
+1. https://medium.com/data-science/breaking-the-enigma-code-in-python-with-mcmc-marvel-themed-9ceb358dd8ae
+2. https://github.com/JackWillz/Projects/tree/master/MCMC%20-%20Enigma%20Thanos
+
+[1]: https://medium.com/data-science/breaking-the-enigma-code-in-python-with-mcmc-marvel-themed-9ceb358dd8ae
---
+++ src/cipher_utils.py
@@ -0,0 +1,54 @@
+import random
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789 ,.?!:;"
+
+def create_encrytpion_dict(alphabet=ALPHABET):
+    """
+    Creates random encryption dictionary for substitution cipher.
+
+    Args:
+        alphabet (str): The string containing all letters used in encryption.
+
+    Returns:
+        dict: A dictionary mapping each letter from the alphabet to a unique
+              substitution letter (also from alphabet)
+    """
+    encryption_dict = {}
+    list_chars = list(alphabet)
+    random.shuffle(list_chars)
+    rand_chars = "".join(list_chars)
+    for i in range(len(alphabet)):
+        encryption_dict[alphabet[i]] = rand_chars[i]
+    return encryption_dict
+
+def encode(text: str, encryption_dict: str):
+    """
+    Encodes a message using a substitution cipher.
+
+    Args:
+        text (str): The message to be encoded.
+        encryption_dict (dict): The dictionary used for encryption.
+
+    Returns:
+        str: The encrypted text.
+    """
+    text_list = list(text)
+    for i in range(len(text_list)):
+        new_char = encryption_dict[text_list[i]]
+        text_list[i] = new_char
+    new_text = "".join(text_list)
+    return new_text
+
+def decode(encoded_text: str, encryption_dict: str):
+    """
+    Decodes a text encoded with a substitution cipher.
+
+    Args:
+        encoded_text (str): The encrypted text.
+        encryption_dict (dict): The dictionary used for encryption.
+
+    Returns:
+        str: The decoded plain text.
+    """
+    decryption_dict = {v: k for k, v in encryption_dict.items()}
+    return encode(encoded_text, decryption_dict)
