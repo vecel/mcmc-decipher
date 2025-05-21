d63a908921cf5383594b97fde59e218cc14de442
iza <01180622@pw.edu.pl>
2025-05-21 19:16:16 +0200

    create new corpora

--- src/language_corpora_init.py
+++ src/language_corpora_init.py
@@ -1,14 +1,24 @@
-from language_utils import create_lang_corpus 
+from language_utils import create_lang_corpus, translate_queries 
 import os
+import asyncio
+
+lang_codes = ["pl", "hu", "sv", "it", "de"]
 
 queries = ["Historia", "Geografia", "Internet", "Muzyka", "Literatura", "Sport", \
-            "Natura", "Wydarzenia", "Polityka", "Taniec", "Zwierzęta", "Nauka", "Człowiek"]
+             "Natura", "Wydarzenia", "Polityka", "Taniec", "Zwierzęta", "Nauka", \
+                "Człowiek", "Film", "Moda", "Architektura", "Religia"]
 
-pl_corpus = create_lang_corpus('pl', 10, queries)
+for lang_code in lang_codes:
+    if lang_code != "pl":
+        queries_t = asyncio.run(translate_queries(queries, src="pl", dest=lang_code))
 
-if not os.path.exists("../data/pl_corpus.txt"):
-    text_file = open("../data/pl_corpus.txt", "w")
-    text_file.write(pl_corpus)
-    text_file.close()
+    new_corpus = create_lang_corpus(lang_code, 10, queries_t)
 
-# TODO other corpora    
+    path = "../data/" + str(lang_code) + "_corpus.txt"
+
+    if not os.path.exists(path):
+        text_file = open(path)
+        text_file.write(new_corpus)
+        text_file.close()
+
+# TODO check if it works   
