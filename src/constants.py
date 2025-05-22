a5c647253f4b47be0a4367c7c281db02644ef2d5
iza <01180622@pw.edu.pl>
2025-05-22 22:08:43 +0200

    add alphabets

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,7 @@
 LANG_CODES = ["pl", "hu", "sv", "it", "de"]
 ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789 ,.?!:;"
+FGN_ALPH = {"pl" : "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż0123456789 ,.?!:;",
+            "hu" : "aábcdeéfghijklmnóöőpqrstuvwxyzz0123456789 ,.?!:;",
+            "sv" : "aåbcdeéfghijklmnoprstuvwxyzäö0123456789 ,.?!:;",
+            "it" : "abcdefghijklmnopqrstuvwxyz0123456789 ,.?!:;",
+            "de" : "aäbcdefghijklmnoöpqrsßtuüvwxyz0123456789 ,.?!:;"}
