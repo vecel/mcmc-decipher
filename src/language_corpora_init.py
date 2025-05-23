5ecf15971cf03314de4a16a88952ef9b4be6f0b6
iza <01180622@pw.edu.pl>
2025-05-23 19:59:28 +0200

    small changes

--- src/language_corpora_init.py
+++ src/language_corpora_init.py
@@ -7,7 +7,7 @@ from corpus_utils import create_perc_dict
 
 queries = ["Historia", "Geografia", "Internet", "Muzyka", "Literatura", "Sport", \
              "Natura", "Wydarzenia", "Polityka", "Taniec", "Zwierzęta", "Nauka", \
-                "Człowiek", "Film", "Moda", "Architektura", "Religia"]
+                "Człowiek", "Film", "Moda", "Architektura", "Religia", "Kosmos"]
 
 for lang_code in LANG_CODES:
     path = "/mnt/c/Users/user/Documents/AZŁM/mcmc-decipher/data/" + str(lang_code) + "_corpus.txt"
@@ -18,18 +18,18 @@ for lang_code in LANG_CODES:
         else:
             queries_t = queries    
 
-        new_corpus = create_lang_corpus(lang_code, 10, queries_t)
-
-        text_file = open(path, "w")
-        text_file.write(new_corpus)
-        text_file.close()
+        new_corpus = create_lang_corpus(lang_code, 15, queries_t)
+        with open(path, 'w') as textfile:
+            textfile.write(new_corpus)
 
     path_dict = "/mnt/c/Users/user/Documents/AZŁM/mcmc-decipher/data/" + str(lang_code) + "_perc_dict.json"
 
     if not os.path.exists(path_dict):
         with open(path) as file:
             corpus = file.read()
-        new_dict = create_perc_dict(corpus, FGN_ALPH[lang_code])
 
+        new_dict = create_perc_dict(corpus[0:2349663], FGN_ALPH[lang_code])
         with open(path_dict, "w") as outfile:
-            json.dump(new_dict, outfile)    
+            json.dump(new_dict, outfile)  
+
+# TODO run again - make corpora the same length after fixing create_perc_dict
