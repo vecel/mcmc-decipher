57e44b2c3a6aca32ea23b177c9370c20006e6674
iza <01180622@pw.edu.pl>
2025-05-23 22:07:29 +0200

    add corpora and dicts

---
+++ src/languages_init.py
@@ -0,0 +1,34 @@
+from language_utils import create_lang_corpus, translate_queries 
+import os
+import asyncio
+from constants import LANG_CODES, FGN_ALPH
+import json
+from corpus_utils import create_perc_dict
+
+queries = ["Historia", "Geografia", "Internet", "Muzyka", "Literatura", "Sport", \
+             "Natura", "Wydarzenia", "Polityka", "Taniec", "Zwierzęta", "Nauka", \
+                "Człowiek", "Film", "Moda", "Architektura", "Religia", "Kosmos"]
+
+for lang_code in LANG_CODES:
+    # FIXME change absolute path to relative using os.getcwd()
+    path = "/mnt/c/Users/user/Documents/AZŁM/mcmc-decipher/data/" + str(lang_code) + "_corpus.txt"
+    
+    if not os.path.exists(path):
+        if lang_code != "pl":
+            queries_t = asyncio.run(translate_queries(queries, src="pl", dest=lang_code))
+        else:
+            queries_t = queries    
+
+        new_corpus = create_lang_corpus(lang_code, 15, queries_t)
+        with open(path, 'w') as textfile:
+            textfile.write(new_corpus)
+
+    path_dict = "/mnt/c/Users/user/Documents/AZŁM/mcmc-decipher/data/" + str(lang_code) + "_perc_dict.json"
+
+    if not os.path.exists(path_dict):
+        with open(path) as file:
+            corpus = file.read()
+
+        new_dict = create_perc_dict(corpus[0:2700000], FGN_ALPH[lang_code])
+        with open(path_dict, "w") as outfile:
+            json.dump(new_dict, outfile)  
