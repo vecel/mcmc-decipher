3efdde92863ae7ef67a94f51e3299fefb1d34837
iza <01180622@pw.edu.pl>
2025-05-26 22:36:55 +0200

    add some letters

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 LANG_CODES = ["pl", "hu", "sv", "it", "de"]
-ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789 ,.?!:;"
-FGN_ALPH = {"pl" : "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż0123456789 ,.?!:;",
-            "hu" : "aábcdeéfghijklmnóöőpqrstuvwxyzz0123456789 ,.?!:;",
-            "sv" : "aåbcdeéfghijklmnoprstuvwxyzäö0123456789 ,.?!:;",
-            "it" : "abcdefghijklmnopqrstuvwxyz0123456789 ,.?!:;",
-            "de" : "aäbcdefghijklmnoöpqrsßtuüvwxyz0123456789 ,.?!:;"}
+ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789 ',.?!:;"
+FGN_ALPH = {"pl" : "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż0123456789 ',.?!:;",
+            "hu" : "aábcdeéfghiíjklmnoóöőpqrstuűüúvwxyzz0123456789 ',.?!:;",
+            "sv" : "aåbcdeéfghijklmnoprstuvwxyzäö0123456789 ',.?!:;",
+            "it" : "aàbcdeèéfghiìjklmnoòópqrstuùvwxyz0123456789 ',.?!:;",
+            "de" : "aäbcdefghijklmnoöpqrsßtuüvwxyz0123456789 ',.?!:;"}
