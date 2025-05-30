79ffc15f832bc04af3be57a92844767b58385b9e
iza <01180622@pw.edu.pl>
2025-05-30 10:06:31 +0200

    small fix

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,6 @@ LANG_CODES = ["pl", "hu", "sv", "it", "de"]
 ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789 ,.?!:;"
 FGN_ALPH = {"pl" : "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż0123456789 ',.?!:;",
             "hu" : "aábcdeéfghiíjklmnoóöőpqrstuűüúvwxyz0123456789 ',.?!:;",
-            "sv" : "aåbcdeéfghijklmnoprstuvwxyzäö0123456789 ',.?!:;",
+            "sv" : "aåbcdeéfghijklmnopqrstuvwxyzäö0123456789 ',.?!:;",
             "it" : "aàbcdeèéfghiìjklmnoòópqrstuùvwxyz0123456789 ',.?!:;",
             "de" : "aäbcdefghijklmnoöpqrsßtuüvwxyz0123456789 ',.?!:;"}
